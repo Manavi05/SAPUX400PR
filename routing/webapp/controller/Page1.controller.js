2041a35f2e2f0b0b064d94f448e09badefb8a7c6
Manavi Pawar <manavipawar9@gmail.com>
2026-04-09 05:02:49 +0000

    added routing

---
+++ routing/webapp/controller/Page1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+
+    return Controller.extend("demo.routing.routing.controller.Page1", {
+        onGoToPage2: function () {
+            this.getOwnerComponent().getRouter().navTo("RoutePage2");
+        }
+    });
+});
